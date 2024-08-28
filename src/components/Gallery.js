8d6f4bc12e17134a5d9e60f8efc79c1ae6345ff1
unvariant <unvariant.winter@gmail.com>
2024-08-28 08:36:45 -0700

    fix loading gallery css

---
+++ src/components/Gallery.js
@@ -0,0 +1,30 @@
+import Footer from "./Footer.mdx";
+import FileEntry from "./FileEntry.js";
+import TopBar from "./TopBar.js";
+import Meta from "./Meta.js";
+import { useInfo } from "./InfoContext.js";
+
+export default function (props) {
+    const info = useInfo();
+
+    return (
+        <html id="_top">
+            <head>
+                <Meta></Meta>
+                <link rel="stylesheet" type="text/css" href="/gallery.css"></link>
+            </head>
+
+            <body>
+                <TopBar></TopBar>
+                
+                <div id="_content" style={{
+                    scrollMarginTop: "48px",
+                }}>
+                    { props.children }
+                </div>
+
+                <Footer />
+            </body>
+        </html>
+    );
+}

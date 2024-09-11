65c7c7dc83d10e3abd683537bcad94ae3d736667
unvariant <unvariant.winter@gmail.com>
2024-09-11 09:08:36 -0700

    add title elements to pages

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,17 +1,15 @@
+import { useInfo } from "./InfoContext.js";
+
 export default function(props) {
+    const info = useInfo();
+
     return (
         <>
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 ></meta>
-
-                {/* im lazy deal with title later */}
-                {/* {props.children.props.title ? (
-                    <title>{props.children.props.title}</title>
-                ) : (
-                    <></>
-                )} */}
+                <title>{ "/" + info.relativePath }</title>
 
                 <link rel="stylesheet" href="/fonts.css" type="text/css" preload></link>
                 <link rel="stylesheet" href="/style.css" type="text/css" preload></link>

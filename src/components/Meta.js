8d3e43f47850aa956ae3caf14bb0650e2ef61649
unvariant <unvariant.winter@gmail.com>
2024-09-10 23:13:53 -0700

    fix div is head element

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,19 +1,19 @@
 export default function(props) {
     return (
-        <div>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-            ></meta>
+        <>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                ></meta>
 
-            {/* im lazy deal with title later */}
-            {/* {props.children.props.title ? (
-                <title>{props.children.props.title}</title>
-            ) : (
-                <></>
-            )} */}
+                {/* im lazy deal with title later */}
+                {/* {props.children.props.title ? (
+                    <title>{props.children.props.title}</title>
+                ) : (
+                    <></>
+                )} */}
 
-            <link rel="stylesheet" href="/style.css"></link>
-        </div>
+                <link rel="stylesheet" href="/style.css"></link>
+        </>
     );
 }

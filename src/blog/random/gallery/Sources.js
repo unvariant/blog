b68067611928e23a5a25907ea64965ab2fc13ece
unvariant <unvariant.winter@gmail.com>
2024-10-02 15:20:53 -0700

    update sources with labels

--- src/blog/random/gallery/Sources.js
+++ src/blog/random/gallery/Sources.js
@@ -1,9 +1,39 @@
-export default function(props) {
-    const sourceLinks = props.links.map(link => (
-        <li>
-            <a href={ link } target="_blank" rel="noopener noreferrer" class="ellipsis">{ link }</a>
-        </li>
-    ));
+export default function (props) {
+    let sourceLinks = <p>Oops! something went wrong.</p>;
+    if (Array.isArray(props.links)) {
+        sourceLinks = props.links.map((link) => (
+            <li>
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="ellipsis"
+                >
+                    {link}
+                </a>
+            </li>
+        ));
+    } else {
+        sourceLinks = Object.entries(props.links)
+            .map(([label, link]) => {
+                if (link !== undefined) {
+                    return (
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="ellipsis"
+                        >
+                            {label}
+                        </a>
+                    );
+                } else {
+                    return <span>{label}</span>;
+                }
+            })
+            .map((e) => <li>{e}</li>);
+    }
+
     return (
         <div>
             <details class="sources fullwidth">
@@ -11,9 +41,7 @@ export default function(props) {
                     <h3>Sources (Click me!)</h3>
                 </summary>
                 <div class="links fullwidth">
-                    <ul>
-                        { sourceLinks }
-                    </ul>
+                    <ul>{sourceLinks}</ul>
                 </div>
             </details>
         </div>

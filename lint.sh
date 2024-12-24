fad3bc6292f0c96c23739dfdee8718ec3b6bb847
unvariant <unvariant.winter@gmail.com>
2024-12-24 12:20:15 -0800

    fmt

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=("package.json" "src/utils" "src/plugins" "src/loaders" "src/bun-loaders" "src/build.js" "src/components.js" "src/highlight.js" "src/shiki.js" "src/processor.js" "src/render.js")
+FILES=("package.json" "src/utils" "src/plugins" "src/loaders" "src/build.js" "src/components.js" "src/highlight.js" "src/shiki.js" "src/processor.js" "src/render.js")
 
 if [[ "check" == "${1}" ]]; then
     echo "checking"
--- src/highlight.js
+++ src/highlight.js
@@ -1,7 +1,7 @@
 import * as shiki from "./shiki.js";
 import * as hljs from "./hljs.js";
 import fs from "node:fs";
-import path from"node:path";
+import path from "node:path";
 import config from "#utils/config.js";
 import crypto from "node:crypto";
 
@@ -13,16 +13,20 @@ const highlightCache = path.join(config.cacheRoot, "highlight-cache");
 fs.mkdirSync(highlightCache, { recursive: true });
 
 /**
- * @param {string} provider 
- * @param {string} code 
- * @param {string} language 
+ * @param {string} provider
+ * @param {string} code
+ * @param {string} language
  * @returns {string}
  */
 export function highlight(provider, code, language) {
     if (providers.hasOwnProperty(provider)) {
-        const key = crypto.createHash("md5").update(code).update(language).digest("hex");
+        const key = crypto
+            .createHash("md5")
+            .update(code)
+            .update(language)
+            .digest("hex");
         const cached = path.join(highlightCache, key);
-    
+
         try {
             return fs.readFileSync(cached, { encoding: "utf-8" });
         } catch (e) {

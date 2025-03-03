ae2426c4d26bf153a0d995c17d9342102a1a6437
unvariant <unvariant.winter@gmail.com>
2025-03-03 11:28:17 -0800

    delete some useless files

--- src/blog/writeups/balsnctf-quals-2023/pwn-baby/test.c
+++
@@ -1,4 +0,0 @@
-int main() {
-    system("");
-    printf("lmao\n");
-}
--- src/blog/writeups/balsnctf-quals-2023/pwn-baby/test.py
+++
@@ -1,7 +0,0 @@
-from pwn import *
-
-libc = ELF("./libc.so.6")
-
-a = libc.sym.__libc_start_call_main
-b = 0x50a37
-print(b - a)
